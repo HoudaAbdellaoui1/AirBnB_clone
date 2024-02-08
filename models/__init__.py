a0ae6a4801f92f574b38e0a758a915054b5322f1
HoudaAbdellaoui1 <abdellaouihouda2@gmail.com>
2024-02-08 17:36:28 +0100

    Fix storage import

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-from .base_model import BaseModel
-from .engine.file_storage import FileStorage
+from models.base_model import BaseModel
+from models.engine.file_storage import FileStorage
 
 storage = FileStorage()
 storage.reload()
