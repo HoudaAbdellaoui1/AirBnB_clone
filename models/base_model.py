e22b6da4879a93dd55023c7e40f1744a15d0c786
HoudaAbdellaoui1 <abdellaouihouda2@gmail.com>
2024-02-08 16:12:32 +0100

    Added init files'

--- models/base_model.py
+++ models/base_model.py
@@ -1,51 +1,38 @@
+#!/usr/bin/python3
 import uuid
 from datetime import datetime
 
 class BaseModel:
-    """Defines all common attributes/methods for other classes."""
-    
-    def __init__(self, *args, **kwargs):
-        """Initialize a new BaseModel.
+    """A base model class providing common attributes and methods."""
 
-        Args:
-            args: Unused.
-            kwargs: A dictionary of keyword arguments.
-        """
-        if kwargs:
-            for key, value in kwargs.items():
-                setattr(self, key, value)
-                if key == 'created_at' or key == 'updated_at':
-                    setattr(self, key, datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f'))
-        else:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-
-    def __str__(self):
-        """Return a string representation of the BaseModel.
-
-        Returns:
-            A string with the format: [<class name>] (<self.id>) <self.__dict__>
-        """
-        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+    def __init__(self):
+        """Initialize a new instance of BaseModel."""
+        self.id = str(uuid.uuid4())  # Assign a unique ID
+        self.created_at = datetime.now()  # Set creation time
+        self.updated_at = datetime.now()  # Set update time
 
     def save(self):
-        """Update the updated_at attribute with the current datetime.
-        """
+        """Update the 'updated_at' attribute with the current datetime."""
         self.updated_at = datetime.now()
 
     def to_dict(self):
-        """Return a dictionary representation with “simple object type” of our BaseModel.
+        """
+        Convert the object attributes to a dictionary.
 
         Returns:
-            A dictionary containing all keys/values of __dict__ of the instance.
-            The keys are the attribute names of the instance and the values are
-            the corresponding attribute values.
-            A key __class__ must be added to this dictionary with the class name of the object.
-            The created_at and updated_at must be converted to string object in ISO format.
+            dict: Dictionary containing object attributes.
         """
         dict_copy = self.__dict__.copy()
-        dict_copy['__class__'] = self.__class__.__name__
+        # Convert datetime objects to ISO format strings
         dict_copy['created_at'] = self.created_at.isoformat()
         dict_copy['updated_at'] = self.updated_at.isoformat()
         return dict_copy
+
+    def __str__(self):
+        """
+        Generate a string representation of the object.
+
+        Returns:
+            str: String representation of the object.
+        """
+        return f"[{self.__class__.__name__}] ({self.id}) {self.to_dict()}"
